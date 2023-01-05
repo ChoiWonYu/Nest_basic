5e7918dac49d5dd8fc80e3fc8a8300a6ab9e127c
ChoiWonYu <phillip07290@naver.com>
2023-01-05 16:56:09 +0900

    feat: signup,signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/AuthCredentialDto';
 
@@ -6,7 +6,11 @@ import { AuthCredentialDto } from './dto/AuthCredentialDto';
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/signup')
-  signUp(@Body() authCredentialDto: AuthCredentialDto) {
+  signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) {
     return this.authService.signUp(authCredentialDto);
+  }
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) {
+    return this.authService.signIn(authCredentialDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/AuthCredentialDto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,5 +11,14 @@ export class AuthService {
   ) {}
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     await this.userRepository.CreateUser(authCredentialDto);
+  }
+  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    const { userName, password } = authCredentialDto;
+    const user = await this.userRepository.findOneBy({ userName });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'LogIn success';
+    } else {
+      throw new UnauthorizedException('Login failed');
+    }
   }
 }
